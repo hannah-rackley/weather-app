bc80e60f13b00745d7a6f2bd50b653551609aa2e
Hannah <hrglasser94@gmail.com>
2018-10-04 13:36:09 -0400

    Made location card a class so I can store the current page location in state.

--- Frontend/src/Components/LocationWeatherCard.js
+++ Frontend/src/Components/LocationWeatherCard.js
@@ -2,15 +2,27 @@ import React from 'react';
 import { connect } from 'react-redux';
 import TodayTemplate from './TodayTemplate';
 
-const LocationWeatherCard = (props) => {
-    let specificLocation = props.location[0].specific;
-    return (
-        <div className='location-weather-card'>
-            <button onClick={() => {props.dispatch({ type: "REMOVE_LOCATION", specific: specificLocation })}}>X</button>
-            <TodayTemplate location={props.location} specific={specificLocation}/>
-            <a href="https://www.yahoo.com/?ilc=401" target="_blank"> <img src="https://poweredby.yahoo.com/purple.png" width="134" height="29"/> </a>
-        </div>
-    )
+class LocationWeatherCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPageToday: "today"
+        }
+    }
+
+    render() {
+        let specificLocation = this.props.location[0].specific;
+        return (
+            <div className='location-weather-card'>
+                <button onClick={() => {this.props.dispatch({ type: "REMOVE_LOCATION", specific: specificLocation })}}>X</button>
+                <button onClick={() => {this.setState({currentPage: "future"})}}>Future</button>
+                <button onClick={() => {this.setState({currentPage: "today"})}}>Current</button>
+                {this.state.currentPage !== "future" ? <TodayTemplate location={this.props.location} specific={specificLocation}/> : <p>{specificLocation} Future!!!</p>}
+                <a href="https://www.yahoo.com/?ilc=401" target="_blank"> <img src="https://poweredby.yahoo.com/purple.png" width="134" height="29"/> </a>
+            </div>
+        )
+    }
+    
 }
 
 const SmartLocationWeatherCard = connect(state => ({dispatch: state.dispatch}))(LocationWeatherCard)
